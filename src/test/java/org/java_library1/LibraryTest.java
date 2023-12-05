5237f824057dfba9be0304980300d3aa8901e6f7
Angelika Kuris <likakuris9@gmail.com>
2023-12-05 14:32:07 +0200

    TestChanges

--- src/test/java/org/java_library1/LibraryTest.java
+++ src/test/java/org/java_library1/LibraryTest.java
@@ -3,6 +3,11 @@ package org.java_library1;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.function.BooleanSupplier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
@@ -23,12 +28,30 @@ class LibraryTest {
 
     @Test
     void displayBooks() {
-    //anyway
+        library.addBook(book);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+
+
+        library.displayBooks();
+
+
+        System.setOut(originalOut);
+
+        assertTrue(outContent.toString().contains(book.getTitle()));
+        assertTrue(outContent.toString().contains(book.getAuthor()));
+        assertTrue(outContent.toString().contains(book.getIsbn()));
+        assertTrue(outContent.toString().contains(String.valueOf(book.getYear())));
     }
+
 
     @Test
     void findBookByTitle() {
-
+        library.addBook(book);
+        assertEquals(book, library.findBookByTitle("Test Book"));
+        assertNull(library.findBookByTitle("Non-existing Book"));
     }
 
     @Test
@@ -40,5 +63,9 @@ class LibraryTest {
 
     @Test
     void isLibraryEmpty() {
+
+        assertTrue(library.isLibraryEmpty());
+        library.addBook(book);
+        assertFalse(library.isLibraryEmpty());
     }
 }
